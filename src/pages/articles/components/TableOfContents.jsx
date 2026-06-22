81ff3a0417e7105c2527d0464423bec73ef326f1
Bardia <iconiccerberus@gmail.com>
2026-06-22 18:01:06 +0330

    article fix

--- src/pages/articles/components/TableOfContents.jsx
+++ src/pages/articles/components/TableOfContents.jsx
@@ -1,10 +1,14 @@
 function TableOfContents({sections}) {
+  const visibleSections = sections.filter((section) => section.showInBody !== false);
+
   return (
     <nav className="article-toc" aria-label="Table of contents">
       <h2>Table of Contents</h2>
       <ol>
-        {sections.map((section) => (
-          <li key={section.id}><a href={`#${section.id}`}>{section.tocLabel ?? section.heading}</a></li>
+        {visibleSections.map((section) => (
+          <li key={section.id}>
+            <a href={`#${section.id}`}>{section.tocLabel ?? section.heading}</a>
+          </li>
         ))}
       </ol>
     </nav>
