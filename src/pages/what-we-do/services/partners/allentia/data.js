98c09861de45a014b9e883666325dc2b760a25f2
Bardia <iconiccerberus@gmail.com>
2026-06-29 15:13:01 +0330

    alleantia

--- src/pages/what-we-do/services/partners/allentia/data.js
+++ src/pages/what-we-do/services/partners/allentia/data.js
@@ -1,5 +1,5 @@
 import logo from "../../../../../assets/images/partners/allentia/allentia.svg";
-import heroImage from "../../../../../assets/images/partners/shared/backgrounds/PartnerIntroImage.webp";
+import heroImage from "../../../../../assets/images/partners/allentia/Alleantia.webp";
 import archeryIcon from "../../../../../assets/images/partners/shared/cards/archery.svg";
 import globeIcon from "../../../../../assets/images/partners/shared/cards/globe.svg";
 import puzzleIcon from "../../../../../assets/images/partners/shared/cards/puzzle.svg";
