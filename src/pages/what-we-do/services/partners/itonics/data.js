7c3aa422d6cce6d55676c82801b051af545e0e70
Bardia <iconiccerberus@gmail.com>
2026-06-29 11:49:28 +0330

    itonics done

--- src/pages/what-we-do/services/partners/itonics/data.js
+++ src/pages/what-we-do/services/partners/itonics/data.js
@@ -1,6 +1,19 @@
 import logo from "../../../../../assets/images/partners/itonics/Itonics.svg";
 
+import InsightGrowthAudience from "../../../../../assets/images/partners/shared/cards/InsightGrowthAudience.svg";
+import SettingsWindow from "../../../../../assets/images/partners/shared/cards/SettingsWindow.svg";
+import puzzle from "../../../../../assets/images/partners/shared/cards/puzzle.svg";
+import DashboardMonitor from "../../../../../assets/images/partners/shared/cards/DashboardMonitor.svg";
+import ProcessesWorkflow from "../../../../../assets/images/partners/shared/cards/ProcessesWorkflow.svg";
+
 export const itonicsAssets = {
   logo,
   logoClassName: "max-h-[212px] max-w-[406px]",
+  cardIcons: {
+    insight: InsightGrowthAudience,
+    sites: SettingsWindow,
+    roadmap: puzzle,
+    reports: DashboardMonitor,
+    processes: ProcessesWorkflow,
+  }
 };
