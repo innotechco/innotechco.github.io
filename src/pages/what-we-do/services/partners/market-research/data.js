725938330965a20cd4f88beb6b26e62077d7afbe
Bardia <iconiccerberus@gmail.com>
2026-07-07 19:42:17 +0330

    adding two pages

---
+++ src/pages/what-we-do/services/partners/market-research/data.js
@@ -0,0 +1,16 @@
+import logo from "../../../../../assets/icons/MarketResearch.svg";
+import heroImage from "../../../../../assets/images/partners/shared/backgrounds/PartnerIntroImage.webp";
+import archeryIcon from "../../../../../assets/images/partners/shared/cards/archery.svg";
+import globeIcon from "../../../../../assets/images/partners/shared/cards/globe.svg";
+import puzzleIcon from "../../../../../assets/images/partners/shared/cards/puzzle.svg";
+
+export const marketResearchAssets = {
+  darkLogoClassName: "brightness-0 invert",
+  logo,
+  heroImage,
+  cardIcons: {
+    archery: archeryIcon,
+    globe: globeIcon,
+    puzzle: puzzleIcon,
+  },
+};
