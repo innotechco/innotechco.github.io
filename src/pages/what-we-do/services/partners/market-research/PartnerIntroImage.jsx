8a3256b5fee615e72fd8fa7131e67c1f8fb2f1bf
Bardia <iconiccerberus@gmail.com>
2026-07-07 20:37:10 +0330

    partners pics fix

---
+++ src/pages/what-we-do/services/partners/market-research/PartnerIntroImage.jsx
@@ -0,0 +1,19 @@
+function PartnerIntroImage({assets, content, className = "", style, wrapperClassName = ""}) {
+  if (!assets.heroImage) return null;
+
+  return (
+    <div
+      className={`relative aspect-[16/9] w-[calc(100%+48px)] -translate-x-6 shrink-0 overflow-hidden md:w-[calc(100%+128px)] md:-translate-x-16 xl:h-[382px] xl:w-[600px] xl:translate-x-0 xl:self-stretch ${wrapperClassName}`}
+    >
+      <img
+        loading="lazy"
+        src={assets.heroImage}
+        alt={`${content.name} platform`}
+        className={`size-full object-cover object-center ${className}`}
+        style={style}
+      />
+    </div>
+  );
+}
+
+export default PartnerIntroImage;
