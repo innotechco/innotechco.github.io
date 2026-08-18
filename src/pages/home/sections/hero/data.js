6e4c925a5175db7d2f2fee93c7f6b0fb1b84024c
Bardia <iconiccerberus@gmail.com>
2026-08-18 15:51:07 +0330

    clean up

--- src/pages/home/sections/hero/data.js
+++
@@ -1,3 +0,0 @@
-import {getHomePage} from "../../../../services/content/homeContent";
-
-export const {hero: heroContent} = getHomePage();
--- src/pages/home/sections/live-insights/data.js
+++
@@ -1,3 +0,0 @@
-import {getHomePage} from "../../../../services/content/homeContent";
-
-export const {liveInsights} = getHomePage();
